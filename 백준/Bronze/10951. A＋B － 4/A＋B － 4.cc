2ca0b7bd79c8a187614ca02fe4af33ff4d51ddc2
Sujin Kim <105408672+sml09181@users.noreply.github.com>
2024-07-04 12:34:04 +0900

    Update A＋B － 4.cc

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -1,9 +1,24 @@
 #include <stdio.h>
 
+//
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int a, b;
+    while (cin >> a >> b) cout << a+b << "\n";
+    return 0;
+}
+
+/*
 int main(void) {
     int a, b;
     while (scanf("%d %d", &a, &b)!=EOF)
         printf("%d \n", a+b);
     return 0;
 }
+*/
 
