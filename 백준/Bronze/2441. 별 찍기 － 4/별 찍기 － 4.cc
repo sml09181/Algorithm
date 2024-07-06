3e8659885fce51ca1c6f193ad0a3d100fb37d01b
Sujin Kim <105408672+sml09181@users.noreply.github.com>
2024-07-06 11:40:51 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    for (int i=n; i>0; i--) {
+        for (int j=0; j<n-i; j++) cout << " ";
+        for (int j=0; j<i; j++) cout << "*";
+        cout << "\n";
+    }
+    return 0;
+}
