e4d3a743e8d93b393c3112015b0867ec678a4f5c
Sujin Kim <105408672+sml09181@users.noreply.github.com>
2024-07-06 11:03:12 +0900

    [Bronze I] Title: 2007년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.cc
+++ 백준/Bronze/1924. 2007년/2007년.cc
@@ -6,58 +6,12 @@ int main(int argc, const char *argv[]) {
     cin.tie(nullptr);
     int x, y;
     cin >> x >> y;
-    int days = y-1;
+    int year[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+    string day[7] = { "SUN","MON","TUE","WED","THU","FRI","SAT" };
+    int sum = y;
     
-    switch (x-1) {
-        case 11:
-            days += 30;
-        case 10:
-            days += 31;
-        case 9:
-            days += 30;
-        case 8:
-            days += 31;
-        case 7:
-            days += 31;
-        case 6:
-            days += 30;    
-        case 5:
-            days += 31;
-        case 4:
-            days += 30;
-        case 3:
-            days += 31;
-        case 2:
-            days += 28;
-        case 1:
-            days += 31;
-        default: break;
-    }
-    days %= 7;
-    
-    switch(days) {
-        case 0:
-            cout << "MON";
-            break;
-        case 1:
-            cout << "TUE";
-            break;
-        case 2:
-            cout << "WED";
-            break;
-        case 3:
-            cout << "THU";
-            break;
-        case 4:
-            cout << "FRI";
-            break;
-        case 5:
-            cout << "SAT";
-            break;
-        case 6:
-            cout << "SUN";
-            break;
-    }
+    for (int i=0; i<x-1; i++) sum+= year[i];
+    cout << day[sum%7];
     return 0;
     
     
