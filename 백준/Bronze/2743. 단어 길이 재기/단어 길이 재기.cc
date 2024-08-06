277b991e30d83a0e889ae4e523aa43f7c212f6a7
Sujin Kim <105408672+sml09181@users.noreply.github.com>
2024-08-06 21:25:44 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char* argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string str;
+    cin >> str;
+    cout << str.length();
+    return 0;
+}
