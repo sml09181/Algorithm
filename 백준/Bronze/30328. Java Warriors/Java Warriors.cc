733688b1e0d2ea7341178b01561f158838cbd9f8
Sujin Kim <105408672+sml09181@users.noreply.github.com>
2024-08-20 10:37:57 +0900

    [Bronze V] Title: Java Warriors, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char* argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin >> n;
+    cout << 4000*n << '\n';
+    
+    return 0;
+}
